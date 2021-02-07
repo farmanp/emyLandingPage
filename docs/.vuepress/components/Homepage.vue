5f2f32bf64c084e7720c1463bbe2c064d0dc8804
Farman Pirzada <farman.pirz@gmail.com>
2021-02-07 16:31:01 -0700

    Vuetilize (#1)
    * Intense progress of restructuring folder and using vue components over readme
    
    * Styling for max width
    
    * Ready for merge

---
+++ docs/.vuepress/components/Homepage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <main aria-labelledby="main-title" class="home">
+      <img class="heroImage" src="../public/abstract-online-shopping.png">
+      <header class="hero"> 
+      <h1 id="main-title">
+          Emy
+      </h1> 
+      
+      <p class="description">
+        Financial empowerment at the tip of your fingers
+      </p> 
+      
+      <p class="action">
+
+        <a href="https://57xqcq5c5x1.typeform.com/to/Jq4wMeTi" class="nav-link action-button">
+          Join the Waitlist →
+        </a>
+      </p>  
+      </header>
+      
+      <h1 style="text-align: center;">COMING SOON</h1>
+      <div class="features">        
+        <div class="feature">
+          <h2>Profile</h2>
+            <p>Create a profile to track your progress</p>
+        </div>
+         
+        <div class="feature">
+          <h2>Report</h2>
+            <p>Generate a comprehensive overview</p>
+        </div>
+            
+        <div class="feature">
+          <h2>Literacy</h2>
+            <p>Educate yourself</p>
+        </div>
+      </div> 
+               
+      <div class="theme-default-content custom content__default">
+      </div> 
+                 
+      <div class="footer">
+        Made by Farman with ❤️
+      </div>
+    </main>  
+  </div>
+</template>
+
+<style>
+  .heroImage {
+  right: 150px;    
+  }
+</style>
